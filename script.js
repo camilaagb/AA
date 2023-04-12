c11bf586b69481f89d85fc3c8100f92216fa9485
camilaagb <camilagoncalves2007@hotmail.com>
2023-04-12 18:56:01 -0300

    Landing Page_Projeto Web

---
+++ script.js
@@ -0,0 +1,124 @@
+$(document).ready(function() {
+    console.log( "ready!" );
+    
+    changeMenu("coffeAndTea");
+
+});
+
+function changeMenu(type){
+
+    switch (type) {
+        case 'coffeAndTea':
+
+            // JQUERY
+            // $('#coffeAndTea').show();
+            // $('#food').hide();
+            // $('#drinks').hide();
+
+            // JAVASCRIPT
+            const coffeAndTeaElem = document.getElementById("coffeAndTea");
+            coffeAndTeaElem.classList.remove('hide_menu');
+            coffeAndTeaElem.classList.add('show_menu');
+
+            const buttonCoffeAndTea = document.getElementById("buttonCoffeAndTea");
+            buttonCoffeAndTea.classList.add('button_active');
+            
+
+            const coffeAndTeaElem1 = document.getElementById("drinks");
+            coffeAndTeaElem1.classList.remove('show_menu');
+            coffeAndTeaElem1.classList.add('hide_menu');
+
+            const buttonFoods = document.getElementById("buttonFoods");
+            buttonFoods.classList.remove('button_active');
+            
+
+            const coffeAndTeaElem2 = document.getElementById("foods");
+            coffeAndTeaElem2.classList.remove('show_menu');
+            coffeAndTeaElem2.classList.add('hide_menu');
+
+            const buttonDrinks = document.getElementById("buttonDrinks");
+            buttonDrinks.classList.remove('button_active');
+
+
+            
+            
+
+
+            console.log("CHEGUEI AQUI COFFE AND TEA");
+            break;
+        case 'foods':
+
+            // JQUERY
+            // $('#coffeAndTea').hide();
+            // $('#food').show();
+            // $('#drinks').hide();
+
+
+            // JAVASCRIPT
+            const foodsElem = document.getElementById("coffeAndTea");
+            foodsElem.classList.remove('show_menu');
+            foodsElem.classList.add('hide_menu');
+
+            const buttonCoffeAndTea1 = document.getElementById("buttonCoffeAndTea");
+            buttonCoffeAndTea1.classList.remove('button_active');
+            
+
+            const foodsElem1 = document.getElementById("drinks");
+            foodsElem1.classList.remove('show_menu');
+            foodsElem1.classList.add('hide_menu');
+
+            const buttonDrinks1 = document.getElementById("buttonDrinks");
+            buttonDrinks1.classList.remove('button_active');
+            
+
+            const foodsElem2 = document.getElementById("foods");
+            foodsElem2.classList.remove('hide_menu');
+            foodsElem2.classList.add('show_menu');
+
+            const buttonFoods1 = document.getElementById("buttonFoods");
+            buttonFoods1.classList.add('button_active');
+            
+
+
+            console.log("CHEGUEI AQUI FOODS");
+            break;
+        case 'drinks':
+
+            // JQUERY
+            // $('#coffeAndTea').hide();
+            // $('#food').hide();
+            // $('#drinks').show();
+
+            // JAVASCRIPT
+            const drinksElem = document.getElementById("coffeAndTea");
+            drinksElem.classList.remove('show_menu');
+            drinksElem.classList.add('hide_menu');
+
+            const buttonCoffeAndTea2 = document.getElementById("buttonCoffeAndTea");
+            buttonCoffeAndTea2.classList.remove('button_active');
+            
+
+            const drinksElem1 = document.getElementById("drinks");
+            drinksElem1.classList.remove('hide_menu');
+            drinksElem1.classList.add('show_menu');
+
+            const buttonDrinks2 = document.getElementById("buttonDrinks");
+            buttonDrinks2.classList.add('button_active');
+            
+
+            const drinksElem2 = document.getElementById("foods");
+            drinksElem2.classList.remove('show_menu');
+            drinksElem2.classList.add('hide_menu');
+
+            const buttonFoods2 = document.getElementById("buttonFoods");
+            buttonFoods2.classList.remove('button_active');
+
+
+            console.log("CHEGUEI AQUI DRINKS");
+            break;
+        default:
+            console.log("CHEGUEI NO DEFAULT");
+            break;
+    }
+
+}
